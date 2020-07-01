85776d552137ba3cdabc75308957d71b42d706d1
bishwajit <38694064+bishwajit1998@users.noreply.github.com>
2020-07-01 06:10:20 +0530

    Update modules.js

--- node module system/modules.js
+++ node module system/modules.js
@@ -1,47 +1,47 @@
-//topic = { path module,os module, fs module }
+topic = { path module,os module, fs module }
+//path module
 
 
+module wrapper function
+(function (exports, require, module, __filename, __dirname)
+............................................
 
-// module wrapper function
-// (function (exports, require, module, __filename, __dirname)
-// ............................................
+pathm module 
+let pathobj = path.parse(__filename);
 
-// pathm module 
-// let pathobj = path.parse(__filename);
+let url = 'http://mylogger.io/log';
 
-// let url = 'http://mylogger.io/log';
+console.log(pathobj);
+//............................................
 
-// console.log(pathobj);
-// ............................................
+//os module 
 
-// os module 
+const os = require('os');
 
-// const os = require('os');
+let totalmem = os.totalmem();
+let freemem = os.freemem();
 
-// let totalmem = os.totalmem();
-// let freemem = os.freemem();
+console.log(`totalmem :  ${totalmem}`);
 
-// console.log(`totalmem :  ${totalmem}`);
+console.log(`freemem :  ${freemem}`);
 
-// console.log(`freemem :  ${freemem}`);
+let mem = totalmem-freemem;
 
-// let mem = totalmem-freemem;
-
-// console.log("remaining memore : "+mem); 
-// console.log("hello world");
+console.log("remaining memore : "+mem); 
+console.log("hello world");
 
 
-// .....................................................
+//.....................................................
 
-// FS module 
+//FS module 
 
-//const fs = require('fs');
+const fs = require('fs');
 
-// const files = fs.readdirSync('./');
-// console.log(files);
+const files = fs.readdirSync('./');
+console.log(files);
 
-// fs.readdir('$',function(err,files) {
-// 	if(err) console.log("error",err);
-// 	else console.log("result",files);
-// });
+fs.readdir('$',function(err,files) {
+	if(err) console.log("error",err);
+ 	else console.log("result",files);
+ });
 
